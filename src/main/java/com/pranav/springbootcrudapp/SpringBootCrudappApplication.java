7bb46800a416c4ef145063e47683c1fb20c6b6fc
Pranav <pranavprakashan07@gmail.com>
2021-09-19 16:03:37 +0530

    created a springboot app

---
+++ src/main/java/com/pranav/springbootcrudapp/SpringBootCrudappApplication.java
@@ -0,0 +1,13 @@
+package com.pranav.springbootcrudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCrudappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pranav/springbootcrudapp/SpringBootCrudappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranav.springbootcrudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
